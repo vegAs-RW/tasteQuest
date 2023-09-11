218c6da5bd1c4a618373d7c02ce3d7862588d184
vegAs-RW <jordan.pouchot2@gmail.com>
2023-09-11 15:25:23 +0200

    update user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true},
-    password: { type: String, required: true}
+    email: { type: String, requried: true },
+    password: { type: String, required: true},
+    allRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }],
+    savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }],
 })
 
 export const UserModel = mongoose.model("users", UserSchema)
