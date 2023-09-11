7abe64b59446d1f92f812b46bee903edebf7dae0
vegAs-RW <jordan.pouchot2@gmail.com>
2023-09-11 15:26:39 +0200

    update user route by adding controllers

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,44 +1,19 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import { UserModel } from "../models/user.model.js"
+
+// Import all user controller
+import { createUser, getUserInfo, connectedUser} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-
-  const user = await UserModel.findOne({ username });
-  if (user) {
-    return res.status(400).json({ message: "Username already exists" });
-  }
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = new UserModel({ username, password: hashedPassword });
-  await newUser.save();
-  res.json({ message: "User registered successfully" });
-});
-
-router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-
-  const user = await UserModel.findOne({ username });
-
-  if (!user) {
-    return res
-      .status(400)
-      .json({ message: "Username or password is incorrect" });
-  }
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) {
-    return res
-      .status(400)
-      .json({ message: "Username or password is incorrect" });
-  }
-  const token = jwt.sign({ id: user._id }, "secret");
-  res.json({ token, userID: user._id });
-});
+router.route("/register").post(createUser);
+router.route("/login").post(connectedUser);
+router.route("/:id").get(getUserInfo);
 
 export { router as userRouter };
+
+
+
 
 export const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
