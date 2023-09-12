3d4d7c9108d8008cbc45fa8ff8342db1cb3f3b47
vegAs-RW <jordan.pouchot2@gmail.com>
2023-09-12 17:49:50 +0200

    rework on db model

--- server/models/recipes.model.js
+++ server/models/recipes.model.js
@@ -1,19 +1,12 @@
 import mongoose from "mongoose";
 
-const IngredientSchema = new mongoose.Schema({
-    name: String,
-    quantity: Number,
-    unit: String
-});
 
 const RecipeSchema = mongoose.Schema({
     title: { type: String, required: true },
     description: {type: String, required: true},
     cookingTime: { type: Number, required: true},
-    servings: { type: Number, required: true },
     instructions: { type: String, required: true },
-    tips: { type: String, required: true },
-    ingredients: [IngredientSchema],
+    ingredients: [{ type: String, required: true}],
     imageUrl: { type: String, required: true },
     userOwner: {
       type: mongoose.Schema.Types.ObjectId,
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,8 +4,8 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true},
     email: { type: String, requried: true },
     password: { type: String, required: true},
-    allRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }],
-    savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }],
+    allRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipes' }],
+    savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipes' }],
 })
 
 export const UserModel = mongoose.model("users", UserSchema)
