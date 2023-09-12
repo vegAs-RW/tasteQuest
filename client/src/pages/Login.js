4b22412364b98624679a1238aeaecf854a6e55a8
vegAs-RW <jordan.pouchot2@gmail.com>
2023-09-12 17:48:10 +0200

    create login and register page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Footer from "../components/Footer";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const result = await axios.post("http://localhost:8000/auth/login", {
+        username,
+        email,
+        password,
+      });
+
+      window.localStorage.setItem("userID", result.data.userID);
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      <div className="auth-container">
+        <form onSubmit={handleSubmit} className="auth-form">
+          <h2>Login</h2>
+          <p>Happy to see you again !</p>
+          <div className="form-group">
+            <input
+              className="form-input"
+              type="text"
+              placeholder="Username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-input"
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-input"
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn">
+            Login
+          </button>
+        </form>
+        <p className="switch-login" onClick={() => navigate("/signup")}>
+          You don't have an account ? Click here !
+        </p>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Login;
---
+++ client/src/pages/Register.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./Register.css";
+import Footer from "../components/Footer";
+
+const Register = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8000/auth/register", {
+        username,
+        password,
+        email,
+      });
+      alert("Registration Completed! Now login.");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="auth-container">
+        <form onSubmit={handleSubmit} className="auth-form">
+          <h2>Sign Up</h2>
+          <p>Join our community !</p>
+          <div className="form-group">
+            <input
+              className="form-input"
+              type="text"
+              placeholder="Username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-input"
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-input"
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn">
+            Register
+          </button>
+        </form>
+        <p className="switch-login" onClick={() => navigate("/login")}>
+          You already have an account ? Click here !
+        </p>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Register;
