77900f11518895fcacf90d9f7fdd543f09b4a937
vegAs-RW <jordan.pouchot2@gmail.com>
2023-09-13 14:06:50 +0200

    add comment

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,7 +2,6 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { UserModel } from "../models/user.model.js";
 
-
 // Create a user when is register
 const createUser = async (req, res) => {
   try {
@@ -18,7 +17,7 @@ const createUser = async (req, res) => {
       email,
     });
     await newUser.save();
-    res.status(200).json({ message: "User registered successfully", newUser});
+    res.status(200).json({ message: "User registered successfully", newUser });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -41,17 +40,25 @@ const loginUser = async (req, res) => {
       .status(400)
       .json({ message: "Username or password is incorrect" });
   }
-  const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {expiresIn: "1d"});
-  res.cookie("token", token).status(200).json({message: "Succesfully Login" , token, userID: user._id})
+  const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  res
+    .cookie("token", token)
+    .status(200)
+    .json({ message: "Succesfully Login", token, userID: user._id });
 };
 
 // Logout
 const logoutUser = async (req, res) => {
   try {
-    res.clearCookie("token", {sameSite: "none", secure: true}).status(200).json({message : "User succesfully logout"})
+    res
+      .clearCookie("token", { sameSite: "none", secure: true })
+      .status(200)
+      .json({ message: "User succesfully logout" });
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
-}
+};
 
 export { createUser, loginUser, logoutUser };
