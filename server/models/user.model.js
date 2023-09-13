5d2e61826f95f405e39b4ec7a75672f50939a651
vegAs-RW <jordan.pouchot2@gmail.com>
2023-09-13 14:08:32 +0200

    rework on user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,10 +2,10 @@ import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true},
-    email: { type: String, requried: true },
+    email: { type: String, requried: true, unique: true },
     password: { type: String, required: true},
-    allRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipes' }],
-    savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipes' }],
+    /*allRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipes' }],
+    savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipes' }],*/
 })
 
 export const UserModel = mongoose.model("users", UserSchema)
