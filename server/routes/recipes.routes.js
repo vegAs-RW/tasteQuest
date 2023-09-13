4ef2e49b8cab5bb7e3a1f1ac37bff1c6063e86a2
vegAs-RW <jordan.pouchot2@gmail.com>
2023-09-13 14:09:05 +0200

    add delete and update routes

--- server/routes/recipes.routes.js
+++ server/routes/recipes.routes.js
@@ -1,14 +1,18 @@
 import express from "express";
 
-import { createRecipe,  getAllRecipes, getOneRecipe, saveRecipe, getSavedRecipes} from "../controllers/recipe.controller.js";
+import { createRecipe,  getAllRecipes, getRecipeDetails, /*saveRecipe, getSavedRecipes,*/ updateRecipe, deleteRecipe, getUserRecipes} from "../controllers/recipe.controller.js";
 
 const router = express.Router();
 
-router.route('/').post(createRecipe)
+router.route('/create').post(createRecipe)
+router.route('/:id').put(updateRecipe)
+router.route('/:id').delete(deleteRecipe)
+router.route('/:id').get(getRecipeDetails)
 router.route('/').get(getAllRecipes)
-router.route('/:id').get(getOneRecipe)
-router.route('/:id').put(saveRecipe)
-router.route('/savedRecipes/:id').get(getSavedRecipes)
+router.route('/user/:userId').get(getUserRecipes)
+/*router.route('/:id').put(saveRecipe)
+router.route('/savedRecipes/:id').get(getSavedRecipes)*/
+
 
 
 export { router as recipeRouter };
