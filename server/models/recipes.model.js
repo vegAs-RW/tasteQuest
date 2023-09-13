1d55d57178c0925b356ee441dc0686edcbc045c9
vegAs-RW <jordan.pouchot2@gmail.com>
2023-09-13 14:08:04 +0200

    rework on recipe model to add userId

--- server/models/recipes.model.js
+++ server/models/recipes.model.js
@@ -1,18 +1,13 @@
 import mongoose from "mongoose";
 
-
 const RecipeSchema = mongoose.Schema({
-    title: { type: String, required: true },
-    description: {type: String, required: true},
-    cookingTime: { type: Number, required: true},
-    instructions: { type: String, required: true },
-    ingredients: [{ type: String, required: true}],
-    imageUrl: { type: String, required: true },
-    userOwner: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-  });
-  
-  export const RecipesModel = mongoose.model("Recipes", RecipeSchema);
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  cookingTime: { type: Number, required: true },
+  instructions: { type: String, required: true },
+  ingredients: [{ type: String, required: true }],
+  imageUrl: { type: String, required: false },
+  userId: { type: String, required: true},
+},{timestamps: true});
+
+export const RecipesModel = mongoose.model("Recipes", RecipeSchema);
