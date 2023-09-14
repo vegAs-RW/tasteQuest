971a337f78575c7ce97463e1c47ed0909dccdbaf
vegAs-RW <jordan.pouchot2@gmail.com>
2023-09-14 18:27:26 +0200

    add navbar, homeCards and HomeBackground components to Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import HomeCards from '../components/HomeCards';
+import Navbar from '../components/Navbar';
+import HomeBackground from '../components/HomeBackground';
 
 const home = () => {
     return (
         <div>
-            Home
+            <Navbar />
+           <HomeBackground />
+            <HomeCards/>
         </div>
     );
 };
