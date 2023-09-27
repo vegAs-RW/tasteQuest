eb29631dec19dd56deaec57f10dcdadf0cec4144
vegAs-RW <jordan.pouchot2@gmail.com>
2023-09-27 11:29:10 +0200

    modify a route path

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,27 @@ import "./App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import CreateRecipe from "./pages/CreateRecipe";
+import Profile from "./pages/Profile";
+import UserRecipe from "./pages/UserRecipe";
+import Footer from "./components/Footer";
+import RecipeDetails from "./pages/RecipeDetails";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Register />} />
-          <Route path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Register />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/create" element={<CreateRecipe />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/myrecipe" element={<UserRecipe />} />
+          <Route exact path="/recipes/:id" element={<RecipeDetails />} />
         </Routes>
       </Router>
+      <Footer/>
     </div>
   );
 }
