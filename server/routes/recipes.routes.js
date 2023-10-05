de5163979b8d34489f3039b13a688c8db687b9b5
vegAs-RW <jordan.pouchot2@gmail.com>
2023-10-05 12:25:27 +0200

    add multer middleware on post route

--- server/routes/recipes.routes.js
+++ server/routes/recipes.routes.js
@@ -4,21 +4,20 @@ import {
   createRecipe,
   getAllRecipes,
   getRecipeDetails,
-  /*saveRecipe, getSavedRecipes,*/ updateRecipe,
+  updateRecipe,
   deleteRecipe,
   getUserRecipes,
 } from "../controllers/recipe.controller.js";
 import { verifyToken } from "../utils/verifyToken.js";
+import {uploadMiddleware} from "../utils/multer-config.js"
 
 const router = express.Router();
 
-router.route("/create").post(verifyToken, createRecipe);
+router.route("/create").post(verifyToken, uploadMiddleware, createRecipe);
 router.route("/:id").put(verifyToken, updateRecipe);
 router.route("/:id").delete(verifyToken, deleteRecipe);
 router.route("/:id").get(verifyToken, getRecipeDetails);
 router.route("/").get(verifyToken, getAllRecipes);
 router.route("/user/:userId").get(verifyToken, getUserRecipes);
-/*router.route('/:id').put(saveRecipe)
-router.route('/savedRecipes/:id').get(getSavedRecipes)*/
 
 export { router as recipeRouter };
