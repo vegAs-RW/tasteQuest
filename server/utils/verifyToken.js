7b4eff30e208f867d9e39075c26e29cf9ac5782b
vegAs-RW <jordan.pouchot2@gmail.com>
2023-10-05 12:24:55 +0200

    modify middleware

--- server/utils/verifyToken.js
+++ server/utils/verifyToken.js
@@ -3,11 +3,11 @@ import jwt from "jsonwebtoken";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
-    return res.status(401).json({ message: "You are not authenticated" });
+    return res.status(401).json({ message: "Not authenticated" });
   }
   jwt.verify(token, process.env.SECRET_KEY, async (err, data) => {
     if (err) {
-      return res.status(403).json({ message: "Invalid token" });
+      return res.status(403).json({ message: "invalid Token" });
     }
     req.userId = data.id;
     console.log("middleware");
