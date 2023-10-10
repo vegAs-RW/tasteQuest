f2af3b3e48c04b9653e5e56c96f7487bb65e63fd
vegAs-RW <jordan.pouchot2@gmail.com>
2023-10-10 11:08:42 +0200

    add isAdmin for moderation

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true},
     email: { type: String, requried: true, unique: true },
     password: { type: String, required: true},
-   /*isAdmin: {type: Boolean, required: true}*/
+   isAdmin: {type: Boolean, default: false}
 })
 
 export const UserModel = mongoose.model("users", UserSchema)
